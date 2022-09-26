553c8dee24c4f1eee8cf46e52837ca509931d312
Nawasan <arikato110011@gmail.com>
2022-09-26 09:19:48 +0700

    up style

--- src/App.svelte
+++ src/App.svelte
@@ -10,14 +10,42 @@
     }
 </script>
 
-<main>
-  <h1 class="m-10">Svelte + vite + Tailwindcss</h1>
+<main class="border-4 border-green-400">
+    <h1 class="p-10 text-4xl title">Svelte + vite + Tailwindcss</h1>
     {#if count >= 0}
-        <button class="bg-green-400 text-white focus:outline-none shadow-md shadow-slate-300 active:translate-y-2 active:shadow-none transition-all" on:click={randomCount}>Count: {count}</button>
+        <button
+            class="bg-green-400 p-2 rounded text-white focus:outline-none shadow-md shadow-slate-300 active:translate-y-2 active:shadow-none transition-all"
+            on:click={randomCount}>Count: {count}</button
+        >
     {:else}
-        <button class="bg-rose-400 text-white focus:outline-none shadow-md shadow-slate-300 active:translate-y-2 active:shadow-none transition-all" on:click={randomCount}>Count: {count}</button>
+        <button
+            class="bg-rose-400 p-2 rounded text-white focus:outline-none shadow-md shadow-slate-300 active:translate-y-2 active:shadow-none transition-all"
+            on:click={randomCount}>Count: {count}</button
+        >
     {/if}
 </main>
 
 <style>
+    main {
+        min-height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        box-sizing: border-box;
+    }
+    .title {
+        animation: roling ease 10s infinite;
+    }
+    @keyframes roling {
+        0% {
+            transform: rotate(10deg);
+        }
+        50% {
+            transform: rotate(-10deg);
+        }
+        100% {
+            transform: rotate(10deg);
+        }
+    }
 </style>
