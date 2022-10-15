4520c6f4f5da23a9343930ed3f5274ee324e1ebb
Nawasan <arikato110011@gmail.com>
2022-10-15 19:20:47 +0700

    add meta tag & update readme

--- README.md
+++ README.md
@@ -3,13 +3,17 @@
   <img align="center" alt="Svelte" height="30" width="40" src="https://github.com/devicons/devicon/raw/master/icons/tailwindcss/tailwindcss-plain.svg">
 </p>
 
-# <p align="center">Svelte + vite + Tailwindcss </p>
+# <p align="center">Svelte & Tailwindcss template </p>
 
 ## $template$
 
 ## Get started 
 - `npm install`
 - `npm run dev`
+
+## Inside it
+
+- Svelte + Vite + Tailwindcss
 
 ## TODOs
 
--- src/App.svelte
+++ src/App.svelte
@@ -1,51 +1,51 @@
 <script>
-    export let count = 0;
-    function randomCount() {
-        console.log(Math.random() * 2);
-        if (Math.random() * 2 > 1) {
-            count += Math.floor(Math.random() * 20);
-        } else {
-            count -= Math.floor(Math.random() * 20);
-        }
+  export let count = 0;
+  function randomCount() {
+    console.log(Math.random() * 2);
+    if (Math.random() * 2 > 1) {
+      count += Math.floor(Math.random() * 20);
+    } else {
+      count -= Math.floor(Math.random() * 20);
     }
+  }
 </script>
 
 <main class="border-4 border-green-400">
-    <h1 class="p-10 text-4xl title">Svelte + vite + Tailwindcss</h1>
-    {#if count >= 0}
-        <button
-            class="bg-green-400 p-2 rounded text-white focus:outline-none shadow-md shadow-slate-300 active:translate-y-2 active:shadow-none transition-all"
-            on:click={randomCount}>Count: {count}</button
-        >
-    {:else}
-        <button
-            class="bg-rose-400 p-2 rounded text-white focus:outline-none shadow-md shadow-slate-300 active:translate-y-2 active:shadow-none transition-all"
-            on:click={randomCount}>Count: {count}</button
-        >
-    {/if}
+  <h1 class="p-10 text-4xl title">Svelte + vite + Tailwindcss</h1>
+  {#if count >= 0}
+    <button
+      class="bg-green-400 p-2 rounded text-white focus:outline-none shadow-md shadow-slate-300 active:translate-y-2 active:shadow-none transition-all"
+      on:click={randomCount}>Count: {count}</button
+    >
+  {:else}
+    <button
+      class="bg-rose-400 p-2 rounded text-white focus:outline-none shadow-md shadow-slate-300 active:translate-y-2 active:shadow-none transition-all"
+      on:click={randomCount}>Count: {count}</button
+    >
+  {/if}
 </main>
 
 <style>
-    main {
-        min-height: 100vh;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        flex-direction: column;
-        box-sizing: border-box;
+  main {
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    box-sizing: border-box;
+  }
+  .title {
+    animation: roling ease 10s infinite;
+  }
+  @keyframes roling {
+    0% {
+      transform: rotate(10deg);
     }
-    .title {
-        animation: roling ease 10s infinite;
+    50% {
+      transform: rotate(-10deg);
     }
-    @keyframes roling {
-        0% {
-            transform: rotate(10deg);
-        }
-        50% {
-            transform: rotate(-10deg);
-        }
-        100% {
-            transform: rotate(10deg);
-        }
+    100% {
+      transform: rotate(10deg);
     }
+  }
 </style>
